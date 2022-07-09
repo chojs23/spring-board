6696a0a7c630c24ba1a13da3e9abd8e078cd953f
neo <chojs990222@gmail.com>
2022-07-09 20:50:51 +0900

    #17 - 게시판 뷰 기본 골격 작성

--- src/test/java/com/neo/springboard/controller/ArticleControllerTest.java
+++ src/test/java/com/neo/springboard/controller/ArticleControllerTest.java
@@ -19,7 +19,7 @@ class ArticleControllerTest {
     public ArticleControllerTest(@Autowired MockMvc mvc) {
         this.mvc = mvc;
     }
-    @Disabled("구현 중")
+
     @DisplayName("[view] [GET] 게시글 리스트(게시판) 페이지 - 정상 호출")
     @Test
     public void givenNothing_whenRequestingArticlesView_thenReturnArticlesView() throws Exception {
@@ -28,8 +28,8 @@ class ArticleControllerTest {
         //When & Then
         mvc.perform(get("/articles"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_HTML))
-                .andExpect(view().name("/articles/index"))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
+                .andExpect(view().name("articles/index"))
                 .andExpect(model().attributeExists("articles"));
     }
     @Disabled("구현 중")
@@ -42,7 +42,7 @@ class ArticleControllerTest {
         mvc.perform(get("/articles/1"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.TEXT_HTML))
-                .andExpect(view().name("/articles/detail"))
+                .andExpect(view().name("articles/detail"))
                 .andExpect(model().attributeExists("article"))
                 .andExpect(model().attributeExists("articleComments"));
     }
@@ -56,7 +56,7 @@ class ArticleControllerTest {
         mvc.perform(get("/articles/search"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.TEXT_HTML))
-                .andExpect(view().name("/articles/search"));
+                .andExpect(view().name("articles/search"));
     }
     @Disabled("구현 중")
     @DisplayName("[view] [GET] 게시글 리스트(게시판) 페이지 - 정상 호출")
@@ -68,6 +68,6 @@ class ArticleControllerTest {
         mvc.perform(get("/articles/search-hashtag"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.TEXT_HTML))
-                .andExpect(view().name("/articles/search-hashtag"));
+                .andExpect(view().name("articles/search-hashtag"));
     }
 }
