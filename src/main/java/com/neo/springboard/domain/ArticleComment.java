9f3a4fd2d5b4755c06b00f4024fbd5be889eef84
neo <chojs990222@gmail.com>
2022-07-10 14:08:33 +0900

    #18 - 리펙토링: 사용 안하는 import 정리

--- src/main/java/com/neo/springboard/domain/ArticleComment.java
+++ src/main/java/com/neo/springboard/domain/ArticleComment.java
@@ -3,14 +3,8 @@ package com.neo.springboard.domain;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
-import org.springframework.data.annotation.CreatedBy;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedBy;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
-import java.time.LocalDateTime;
 import java.util.Objects;
 
 
@@ -22,23 +16,28 @@ import java.util.Objects;
         @Index(columnList = "createdBy")
 })
 @Entity
-public class ArticleComment extends AuditingFields{
+public class ArticleComment extends AuditingFields {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Setter @ManyToOne(optional = false) private Article article; // 게시글 (ID)
-    @Setter @Column(nullable = false,length = 500) private String content; // 본문
+    @Setter
+    @ManyToOne(optional = false)
+    private Article article; // 게시글 (ID)
+    @Setter
+    @Column(nullable = false, length = 500)
+    private String content; // 본문
 
-    
-    protected ArticleComment() {}
+
+    protected ArticleComment() {
+    }
 
     private ArticleComment(Article article, String content) {
         this.article = article;
         this.content = content;
     }
 
-    public static  ArticleComment of(Article article, String content) {
+    public static ArticleComment of(Article article, String content) {
         return new ArticleComment(article, content);
     }
 
@@ -46,7 +45,7 @@ public class ArticleComment extends AuditingFields{
     public boolean equals(Object o) {
         if (this == o) return true;
         if (!(o instanceof ArticleComment that)) return false;
-        return id!=null && id.equals(that.id);
+        return id != null && id.equals(that.id);
     }
 
     @Override
