5fd59c44978b562e1030a785fd45cea460549268
neo <chojs990222@gmail.com>
2022-07-10 18:04:19 +0900

    #21 - 도메인 dto 생성

--- src/main/java/com/neo/springboard/dto/ArticleCommentDto.java
+++ src/main/java/com/neo/springboard/dto/ArticleCommentDto.java
@@ -1,5 +1,6 @@
 package com.neo.springboard.dto;
 
+<<<<<<< Updated upstream
 import java.time.LocalDateTime;
 
 public record ArticleCommentDto(
@@ -12,4 +13,47 @@ public record ArticleCommentDto(
     public static ArticleCommentDto of(LocalDateTime createdAt, String createdBy, LocalDateTime modifiedAt, String modifiedBy, String content) {
         return new ArticleCommentDto(createdAt, createdBy, modifiedAt, modifiedBy, content);
     }
+=======
+import com.neo.springboard.domain.Article;
+import com.neo.springboard.domain.ArticleComment;
+
+import java.time.LocalDateTime;
+
+public record ArticleCommentDto(
+        Long id,
+        Long articleId,
+        UserAccountDto userAccountDto,
+        String content,
+        LocalDateTime createdAt,
+        String createdBy,
+        LocalDateTime modifiedAt,
+        String modifiedBy
+) {
+    public static ArticleCommentDto of(Long id, Long articleId, UserAccountDto userAccountDto, String content, LocalDateTime createdAt, String createdBy, LocalDateTime modifiedAt, String modifiedBy) {
+        return new ArticleCommentDto(id, articleId, userAccountDto, content, createdAt, createdBy, modifiedAt, modifiedBy);
+    }
+
+    public static ArticleCommentDto from(ArticleComment entity) {
+        return new ArticleCommentDto(
+                entity.getId(),
+                entity.getArticle().getId(),
+                UserAccountDto.from(entity.getUserAccount()),
+                entity.getContent(),
+                entity.getCreatedAt(),
+                entity.getCreatedBy(),
+                entity.getModifiedAt(),
+                entity.getModifiedBy()
+        );
+    }
+
+    public ArticleComment toEntity(Article entity) {
+        return ArticleComment.of(
+                entity,
+                userAccountDto.toEntity(),
+                content
+        );
+    }
+
+
+>>>>>>> Stashed changes
 }
--- src/main/java/com/neo/springboard/dto/ArticleDto.java
+++ src/main/java/com/neo/springboard/dto/ArticleDto.java
@@ -1,5 +1,6 @@
 package com.neo.springboard.dto;
 
+<<<<<<< Updated upstream
 import java.time.LocalDateTime;
 
 public record ArticleDto(
@@ -11,6 +12,48 @@ public record ArticleDto(
 ) {
     public static ArticleDto of(LocalDateTime createdAt, String createdBy, String title, String content, String hashtag) {
         return new ArticleDto(createdAt, createdBy, title, content, hashtag);
+=======
+import com.neo.springboard.domain.Article;
+
+import java.time.LocalDateTime;
+
+public record ArticleDto(
+        Long id,
+        UserAccountDto userAccountDto,
+        String title,
+        String content,
+        String hashtag,
+        LocalDateTime createdAt,
+        String createdBy,
+        LocalDateTime modifiedAt,
+        String modifiedBy
+) {
+    public static ArticleDto of(Long id, UserAccountDto userAccountDto, String title, String content, String hashtag, LocalDateTime createdAt, String createdBy, LocalDateTime modifiedAt, String modifiedBy) {
+        return new ArticleDto(id, userAccountDto, title, content, hashtag, createdAt, createdBy, modifiedAt, modifiedBy);
+    }
+
+    public static ArticleDto from(Article entity) {
+        return new ArticleDto(
+                entity.getId(),
+                UserAccountDto.from(entity.getUserAccount()),
+                entity.getTitle(),
+                entity.getContent(),
+                entity.getHashtag(),
+                entity.getCreatedAt(),
+                entity.getCreatedBy(),
+                entity.getModifiedAt(),
+                entity.getModifiedBy()
+        );
+    }
+
+    public Article toEntity() {
+        return Article.of(
+                userAccountDto.toEntity(),
+                title,
+                content,
+                hashtag
+        );
+>>>>>>> Stashed changes
     }
 
 
