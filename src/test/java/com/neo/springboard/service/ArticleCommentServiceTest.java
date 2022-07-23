092dac1688557bef94330d4b1bc921b6528e86b3
neo <chojs990222@gmail.com>
2022-07-23 17:43:08 +0900

    #33 - 댓글 저장, 삭제 기능을 위한 컨트롤러와 서비스 코드 구현

--- src/test/java/com/neo/springboard/service/ArticleCommentServiceTest.java
+++ src/test/java/com/neo/springboard/service/ArticleCommentServiceTest.java
@@ -7,6 +7,7 @@ import com.neo.springboard.dto.ArticleCommentDto;
 import com.neo.springboard.dto.UserAccountDto;
 import com.neo.springboard.repository.ArticleCommentRepository;
 import com.neo.springboard.repository.ArticleRepository;
+import com.neo.springboard.repository.UserAccountRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -33,6 +34,8 @@ class ArticleCommentServiceTest {
     private ArticleRepository articleRepository;
     @Mock
     private ArticleCommentRepository articleCommentRepository;
+    @Mock
+    private UserAccountRepository userAccountRepository;
 
     @DisplayName("게시글 ID로 조회하면, 해당하는 댓글 리스트를 반환한다.")
     @Test
@@ -59,6 +62,7 @@ class ArticleCommentServiceTest {
         // Given
         ArticleCommentDto dto = createArticleCommentDto("댓글");
         given(articleRepository.getReferenceById(dto.articleId())).willReturn(createArticle());
+        given(userAccountRepository.getReferenceById(dto.userAccountDto().userId())).willReturn(createUserAccount());
         given(articleCommentRepository.save(any(ArticleComment.class))).willReturn(null);
 
         // When
@@ -66,6 +70,7 @@ class ArticleCommentServiceTest {
 
         // Then
         then(articleRepository).should().getReferenceById(dto.articleId());
+        then(userAccountRepository).should().getReferenceById(dto.userAccountDto().userId());
         then(articleCommentRepository).should().save(any(ArticleComment.class));
     }
 
@@ -81,6 +86,7 @@ class ArticleCommentServiceTest {
 
         // Then
         then(articleRepository).should().getReferenceById(dto.articleId());
+        then(userAccountRepository).shouldHaveNoInteractions();
         then(articleCommentRepository).shouldHaveNoInteractions();
     }
 
