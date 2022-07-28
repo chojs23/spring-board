6ef2504fd5c2f9a09e24a6e5bc811d8b8d3faf18
neo <chojs990222@gmail.com>
2022-07-28 19:04:43 +0900

    #43 - 시큐리티 설정 자바 대응 코드 구현

---
+++ src/test/java/com/neo/springboard/config/TestSecurityConfig.java
@@ -0,0 +1,30 @@
+package com.neo.springboard.config;
+
+import com.neo.springboard.domain.UserAccount;
+import com.neo.springboard.repository.UserAccountRepository;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.event.annotation.BeforeTestMethod;
+
+import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.given;
+
+@Import(SecurityConfig.class)
+public class TestSecurityConfig {
+    @MockBean
+    private UserAccountRepository userAccountRepository;
+
+    @BeforeTestMethod
+    public void securitySetUp() {
+        given(userAccountRepository.findById(anyString())).willReturn(Optional.of(UserAccount.of(
+                "neoTest",
+                "pw",
+                "neo@email.com",
+                "neo-test",
+                "test memo"
+        )));
+
+    }
+}
